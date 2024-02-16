f95d7a0eceb11ec20d4fd91fc797d4e5749608e3
nikolaradon <nikola.radon@gmail.com>
2024-02-16 18:42:39 +0100

    updated views MovieView and MovieListView

--- cinemaproject/cinemanager_app/views.py
+++ cinemaproject/cinemanager_app/views.py
@@ -2,16 +2,49 @@ from django.shortcuts import render
 from .models import Movie
 from .serializers import MovieSerializer
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
 
 
 
-class MovieListView(generics.ListCreateAPIView):
-    queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
+class MovieListView(APIView):
+    def get(self, request):
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many='true')
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = MovieSerializer(movies, many='true')
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class MovieView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
+class MovieView(APIView):
+    def get_object(self, pk):
+        try:
+            return Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            raise status.HTTP_404_NOT_FOUND
 
+    def get(self, request):
+        movie = self.get_object(pk)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+
+    def put(self, request):
+        movie = self.objects.get(pk=pk)
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def delete(self, request):
+        movie = self.get_object(pk=pk)
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
