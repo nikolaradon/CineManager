f6fa21aaf3b7bd76a523abd7ddf8fc505e58f43f
nikolaradon <nikola.radon@gmail.com>
2024-02-16 18:59:45 +0100

    .

--- cinemaproject/cinemanager_app/models.py
+++ cinemaproject/cinemanager_app/models.py
@@ -16,13 +16,3 @@ class Movie(models.Model):
     actors = models.ManyToManyField(Person, related_name="movies_cast")
 
 
-class Cinema(models.Model):
-    name = models.CharField(max_length=255)
-    city = models.CharField(max_length=255)
-    movies = models.ManyToManyField(Movie, through='Screening')
-
-
-class Screening(models.Model):
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    cinema = models.ForeignKey(Cinema, on_delete=models.CASCADE)
-    date = models.DateTimeField
--- cinemaproject/showtimes/models.py
+++ cinemaproject/showtimes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from cinemanager_app.models import Movie, Cinema
 
 # Create your models here.
+class Cinema(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    movies = models.ManyToManyField(Movie, through='Screening')
+
+
+class Screening(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    cinema = models.ForeignKey(Cinema, on_delete=models.CASCADE)
+    date = models.DateTimeField
